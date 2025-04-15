f52dc06eac10df3d8bef1c71d5beba55047a71ee
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2025-04-15 11:38:55 -0300

    File of project

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import Movie from './pages/Movie.jsx';
+import Search from './pages/Search.jsx';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
